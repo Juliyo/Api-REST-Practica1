8757e306323061d474b5bfb5b5705cbae950cf99
Juliyo <julio17795@hotmail.com>
2015-10-10 00:05:53 +0200

    Terminado el CRUD para estaciones

---
+++ ApiREST/models/estaciones.js
@@ -0,0 +1,12 @@
+﻿var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var estacionesSchema = new Schema({
+    _id: Number,
+    identificadorLector: { type: Number },
+    latitud: { type: Number },
+    longitud: { type: Number }
+});
+
+
+module.exports = mongoose.model('Estaciones', estacionesSchema);
---
+++ ApiREST/routes/routes.js
@@ -0,0 +1,93 @@
+﻿//File: routes/tvshows.js
+module.exports = function (app) {
+    
+    var Estaciones = require('../models/estaciones.js');
+    
+    //GET - Return all tvshows in the DB
+    findAllEstaciones = function (req, res) {
+        Estaciones.find(function (err, estacion) {
+            if (!err) {
+                console.log('GET /estaciones')
+                res.send(estacion);
+            } else {
+                console.log('ERROR: ' + err);
+            }
+        });
+    };
+    
+    //GET - Return a TVShow with specified ID
+    findById = function (req, res) {
+        Estaciones.findById(req.params.id, function (err, estacion) {
+            if (!err) {
+                console.log('GET /estaciones/' + req.params.id);
+                res.send(estacion);
+            } else {
+                console.log('ERROR: ' + err);
+            }
+        });
+    };
+    
+    //POST - Insert a new TVShow in the DB
+    addEstacion = function (req, res) {
+        console.log('POST');
+        console.log(req.body);
+        
+        var estacion = new Estaciones({
+            _id:req.body.identificadorLector,
+            identificadorLector: req.body.identificadorLector,
+            latitud: req.body.latitud,
+            longitud: req.body.longitud
+        });
+        
+        estacion.save(function (err) {
+            if (!err) {
+                res.send(estacion);
+                console.log('Created');
+            } else {
+                console.log('ERROR: ' + err);
+                res.send('ERROR: ' + err);
+            }
+        });
+        
+        
+    };
+    
+    //PUT - Update a register already exists
+    updateEstacion = function (req, res) {
+        Estaciones.findById(req.params.id, function (err, estacion) {
+            estacion.identificadorLector = req.body.identificadorLector;
+            estacion.latitud=req.body.latitud;
+            estacion.longitud = req.body.longitud;
+            
+            estacion.save(function (err) {
+                if (!err) {
+                    console.log('Updated');
+                } else {
+                    console.log('ERROR: ' + err);
+                }
+                res.send(estacion);
+            });
+        });
+    }
+    
+    //DELETE - Delete a TVShow with specified ID
+    deleteEstacion = function (req, res) {
+        Estaciones.findById(req.params.id, function (err, estacion) {
+            estacion.remove(function (err) {
+                if (!err) {
+                    console.log('Removed');
+                } else {
+                    console.log('ERROR: ' + err);
+                }
+            })
+        });
+    }
+    
+    //Link routes and functions
+    app.get('/estaciones', findAllEstaciones);
+    app.get('/estaciones/:id', findById);
+    app.post('/estaciones', addEstacion);
+    app.put('/estaciones/:id', updateEstacion);
+    app.delete('/estaciones/:id', deleteEstacion);
+
+}
