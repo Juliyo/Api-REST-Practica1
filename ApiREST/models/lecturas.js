c462494215ea0b4618c137479d3b305f52b67dea
Juliyo <julio17795@hotmail.com>
2015-10-10 16:00:24 +0200

    nada nuevo

---
+++ ApiREST/models/lecturas.js
@@ -0,0 +1,14 @@
+﻿var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var lecturasSchema = new Schema({
+    _id: Number,
+    identificadorIndividuo: { type: Number },
+    identificadorLector: { type: Number },
+    fechaHora: { type: String },
+    latitud: { type: Number },
+    longitud: { type: Number }
+});
+
+
+module.exports = mongoose.model('Lecturas', lecturasSchema);
